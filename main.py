34dc0f754a4821cf0a23125eb3f24cf1f721551c
Arseniy Chernov <chernov.arseny@yandex.ru>
2021-03-14 19:23:01 +0300

    added bg

--- main.py
+++ main.py
@@ -2,14 +2,31 @@ import pygame
 
 run = True
 
+height = 600
+width = 800
+
+class Objects(pygame.sprite.Sprite):
+    def __init__(self, img, x, y, speed):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = img
+        self.rect = self.image.get_rect()
+        self.rect.x = x 
+        self.rect.y = y 
+        self.speed = speed 
 
 
-window = pygame.display.set_mode((480,240))
+bg = pygame.transform.scale(pygame.image.load("img/bg.png"),(width,height))
+
+window = pygame.display.set_mode((800,600))
 pygame.display.set_caption("Игроком")
 while run:
+
+    window.blit(bg, (0, 0))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+
 
 
 
