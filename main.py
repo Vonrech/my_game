cf4e816ceb99a9a3247bbf984bc2fafc7c6870d8
Arseniy Chernov <chernov.arseny@yandex.ru>
2021-03-21 19:02:23 +0300

    added player

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ run = True
 height = 600
 width = 800
 
-class Objects(pygame.sprite.Sprite):
+class Object(pygame.sprite.Sprite):
     def __init__(self, img, x, y, speed):
         pygame.sprite.Sprite.__init__(self)
         self.image = img
@@ -15,10 +15,49 @@ class Objects(pygame.sprite.Sprite):
         self.speed = speed 
 
 
-bg = pygame.transform.scale(pygame.image.load("img/bg.png"),(width,height))
+
 
 window = pygame.display.set_mode((800,600))
-pygame.display.set_caption("Игроком")
+pygame.display.set_caption("Amongus vs Asteroids")
+
+
+#Pictures import
+
+bg = pygame.transform.scale(pygame.image.load("image/bg.png"),(width,height))
+
+player_img = pygame.transform.scale(pygame.image.load("image/player.png"),(64,64))
+
+#Player Spawn
+
+start_x = 100
+start_y = 120 
+
+
+
+
+#Creating groups
+
+all_sprites = pygame.sprite.Group()
+
+
+
+#Creating objects 
+
+player = Object(player_img, start_x, start_y, 3)
+all_sprites.add(player) 
+
+
+
+
+
+
+
+
+
+
+
+
+
 while run:
 
     window.blit(bg, (0, 0))
@@ -29,6 +68,22 @@ while run:
 
 
 
+    keys = pygame.key.get_pressed()
+
+    if event.type == pygame.KEYDOWN:
+        if keys[pygame.K_UP]:
+            player.rect.y -= player.speed
+        if keys[pygame.K_DOWN]:
+            player.rect.y += player.speed
+        if keys[pygame.K_RIGHT]:
+            player.image = pygame.transform.flip(player_img,False,False)
+            player.rect.x += player.speed
+        if keys[pygame.K_LEFT]:
+            player.image = pygame.transform.flip(player_img,True,False)
+            player.rect.x -= player.speed
 
 
+    all_sprites.draw(window)
+    all_sprites.update()
+    
     pygame.display.update()
